53abc7f5a62e3b46e5da7d6812fba9ba642ef7ad
Ana <anasouza673@gmail.com>
2023-11-28 09:57:26 -0300

    atualizado 25/11

--- rotas.js
+++ rotas.js
@@ -1,10 +1,8 @@
 const express = require('express')
 const { listarCategorias } = require('./usuario')
-//const { verificarCamposObrigatorios } = require('./validacoes')
 
 const rotas = express()
 
-//rotas.post("/usuario", verificarCamposObrigatorios, cadastrarUsuario)
 rotas.get('/categoria', listarCategorias)
 
 module.exports = {
