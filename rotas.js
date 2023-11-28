ae99bb54761cf5c0e879ca2dea832a2fc0c090b9
Ana <anasouza673@gmail.com>
2023-11-28 09:48:29 -0300

    atualizado 24/11

--- rotas.js
+++ rotas.js
@@ -1,8 +1,10 @@
 const express = require('express')
-const { listarCategorias } = require('./usuario')
+const { listarCategorias, cadastrarUsuario } = require('./usuario')
+const { verificarCamposObrigatorios } = require('./validacoes')
 
 const rotas = express()
 
+rotas.post("/usuario", verificarCamposObrigatorios, cadastrarUsuario)
 rotas.get('/categoria', listarCategorias)
 
 module.exports = {
--- usuario.js
+++ usuario.js
@@ -1,5 +1,39 @@
 const knex = require("./conexao")
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const senhaToken = require("../senhaToken");
 
+const cadastrarUsuario = async (req, res) => {
+  const { nome, email, senha} = req.body;
+
+  try {
+      const quantidadeUsuarios = await knex('usuarios').where('email',email).first()
+
+      if (quantidadeUsuarios) {
+          return res.status(400).json("O email já existe");
+      }
+
+      const senhaCriptografada = await bcrypt.hash(senha, 10);
+
+
+      const usuario = await knex('usuarios').insert({ 
+        nome: nome, 
+        email: email, 
+        senha: senhaCriptografada 
+      }).returning('*');
+      
+  
+      if(!usuario[0]){
+         return res.status(400).json("O usuario não foi cadastrado")
+      }
+
+      const { senha: _, ...usuarioCadastrado } = usuario[0]
+
+      return res.status(200).json(usuarioCadastrado);
+  }catch(error) {
+      return res.status(400).json(error.message);
+  }
+}
 
 const listarCategorias = async (req, res) => {
 
@@ -20,5 +54,6 @@ const listarCategorias = async (req, res) => {
   }
 
   module.exports = {
-    listarCategorias
+    listarCategorias,
+    cadastrarUsuario
   }
